94c0d6326006e9593f4dd36bfe45aa7257b14902
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-06-26 14:59:30 -0700

    update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   //eslint-disable-next-line no-empty
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    // This block intentionally left empty
   }
 
   return [task, task2];
