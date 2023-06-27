6dd83301f30c11cb51593036500ddc34178adac2
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-06-27 09:15:33 -0700

    update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,8 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, user]) => {
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    .then((user) => {
+      console.log(`${user[0].body} ${user[1].firstName} ${user[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
