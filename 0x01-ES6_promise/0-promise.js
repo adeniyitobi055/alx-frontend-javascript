8944a6ef4f6932791d10c13744abf7d06e5b9865
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-06-27 04:50:52 -0700

    update 0-promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,12 +2,9 @@ export default function getResponseFromAPI() {
   return new Promise((resolver) => {
     // Simulating an asynchronous API call
     setTimeout(() => {
-    // Assuming the API call is succesful
+      // Assuming the API call is succesful
       const responseData = { message: 'API response'};
       resolver(responseData);
-
-      // If the API call encounters an error, use `reject` instead
-      // reject(new Error("API error"));
     }, 2000);
   });
 }
