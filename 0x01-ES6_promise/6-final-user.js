815323abe6a131fbb098ac0befd6470d3eca4e33
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-06-27 10:25:07 -0700

    update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,13 +3,13 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
+g const photoPromise = uploadPhoto(fileName);
 
   return Promise.allSettled([userPromise, photoPromise])
-    .then(results => {
-      return results.map(result => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason.message
-      }));
+    .then(([userResult, photoResult]) => {
+      const user = userResult.status === 'fulfilled' ? userResult.value : null;
+      const photoError = photoResult.status === 'rejected' ? photoResult.reason : null;
+
+      return [{ user, photoError }];
     });
 }
