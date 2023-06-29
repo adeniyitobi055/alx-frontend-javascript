e5aaae3415e308c166da3b704755edcbd717bd70
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-06-29 04:21:54 -0700

    update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  const room1 = new ClassRoom(19);
-  const room2 = new ClassRoom(20);
-  const room3 = new ClassRoom(34);
+  const room_1 = new ClassRoom(19);
+  const room_2 = new ClassRoom(20);
+  const room_3 = new ClassRoom(34);
 
-  return [ room1, room2, room3 ];
+  return [ room_1, room_2, room_3 ];
 }
