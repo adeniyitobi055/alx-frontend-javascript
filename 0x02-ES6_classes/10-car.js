b878971f0df4f778cef6ba6c7c0f23aa84242702
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-06-29 09:56:48 -0700

    update 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -6,7 +6,7 @@ export default class Car {
   }
 
   cloneCar() {
-    const constructor = Reflect.getPrototypeOf(this);
+    const constructor = Reflect.getPrototypeOf(this).Car;
     const { _brand, _motor, _color } = this;
     return new constructor(_brand, _motor, _color);
   }
