1b779e2aefd5a300087ec3bc93e35b87d7541e3d
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-06-29 09:08:21 -0700

    add 100-evcar.js

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,13 @@
+import Car from './10-car.js';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    const { _brand, _motor, _color, _range } = this;
+    return new Car(_brand, _motor, _color);
+  }
+}
