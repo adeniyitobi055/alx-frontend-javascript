052cdabe0f8626fdc74814f7d8f15ff14717ba88
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-07-04 02:18:10 -0700

    add 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,7 +4,7 @@ function queryAPI(endpoint) {
   const count = weakMap.get(endpoint) || 0;
   weakMap.set(endpoint, count + 1);
 
-  if (count >= 5) {
+  if (count >= 4) {
     throw new Error('Endpoint load is high');
   }
 }
