2edf1fbbf176bd9c1fc321b8ea940831c957be17
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-07-05 06:38:44 -0700

    update main.ts for task 3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -30,3 +30,14 @@ class Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  const firstInitial = firstName.charAt(0).toUpperCase();
+  const formattedLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+
+  return `${firstInitial}. ${formattedLastName}`;
+}
