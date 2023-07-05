41c3d587ff5933c79c64771eaf7cc0245490af3f
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-07-05 06:20:26 -0700

    add main.ts

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,28 @@
+class Teacher {
+  private firstName: string;
+  private lastName: string;
+  private fullTimeEmployee: boolean;
+  private yearsOfExperience?: number;
+  private location: string;
+  private additionalAttributes: { [key: string]: any };
+
+  constructor(firstName: string, lastName: string, fullTimeEmployee: boolean, location: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.fullTimeEmployee = fullTimeEmployee;
+    this.location = location;
+    this.additionalAttributes = [];
+  }
+
+  public setYearsOfExperience(yearsOfExperience: number): void {
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  public addAttribute(name: string, value: any): void {
+    this.additionalAttributes[name] = value;
+  }
+
+  public getAttribute(name: string): any {
+    return this.additionalAttributes[name];
+  }
+}
